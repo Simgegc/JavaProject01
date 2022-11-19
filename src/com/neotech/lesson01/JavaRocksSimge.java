88aa42d85eb0b298de5182c6c5cb8fe95bff8649
JennSanginov <jsanginov@gmail.com>
2022-11-19 16:58:22 -0700

    First changes to your project.

---
+++ src/com/neotech/lesson01/JavaRocksSimge.java
@@ -0,0 +1,7 @@
+package com.neotech.lesson01;
+
+public class JavaRocksSimge {
+	////Let me know if you get these changes. 
+	/// Not sure If I am doing this correctly. We are doing so good :-))
+
+}
