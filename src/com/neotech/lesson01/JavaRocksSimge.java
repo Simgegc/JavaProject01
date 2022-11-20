244ff11656a51cb1a8f54c8af10fad26a70c622c
simgegc <avsimgegocer@gmail.com>
2022-11-19 17:32:34 -0800

    this makes me patient:))

--- src/com/neotech/lesson01/JavaRocksSimge.java
+++ src/com/neotech/lesson01/JavaRocksSimge.java
@@ -5,3 +5,5 @@ public class JavaRocksSimge {
 	/// Not sure If I am doing this correctly. We are doing so good :-))
 
 }
+   /// how many hours passed i do not know. i learn very slowly. finally i did:))
+//thank you for your patience:))
